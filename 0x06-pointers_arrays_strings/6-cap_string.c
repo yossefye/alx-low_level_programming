b0a35cfccc178c32b67d158582fd392a6d52cce9
yossefye <yosefyenenehr6@gmail.com>
2023-09-22 01:48:21 -0700

    das

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes letters to upper case
+ * @s: string
+ * Return: shit
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (s);
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 'a' || s[i] >= 'z')
+		{
+			i++;
+			continue;
+		}
+		s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
