1ee7a4de26cd18c4db64b0ae9a020bfa58c0038f
yossefye <yosefyenenehr6@gmail.com>
2023-09-25 13:04:07 -0700

    new

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,21 @@
-#include "main.h"
-/**
- * _memcpy - a function that copies memory area from a source to a destination
- * @dest: the destination
- * @src: the source
- * @n: the numbers of the memory area to be copied
- * Return: the destination
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
+#include "holberton.h"
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer where we search for charachter
+ * @c: character we search for
+ * Return: NULL if character is not found, return pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *s1 = s - 1;
+
+	do {
+		s1++;
+		if (*s1 == c)
+			return (s1);
+	} while (*s1 != '\0');
+
+	return (0);
 }
